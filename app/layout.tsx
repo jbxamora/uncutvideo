f94f4ba18e35717ebbf791cb5ccbd98e87c52c31
jbxamora <jbxamora@icloud.com>
2023-06-12 23:33:44 -0700

    coming soon page deconstructed privport

---
+++ app/layout.tsx
@@ -0,0 +1,77 @@
+import "../global.css";
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Uncut Video | Film & Photography",
+    template: "%s | uncut.video",
+  },
+  description:
+    "Unleashing creativity through an uncut vision of film and photography. We're creating and sharing raw, unfiltered visual stories.",
+  openGraph: {
+    title: "Uncut Video | Film & Photography",
+    description:
+      "Specializing in unedited and unfiltered film and photography. We showcase raw talent and authentic visual narratives in their purest form.",
+    url: "https://uncut.video",
+    siteName: "Uncut Video",
+    images: [
+      {
+        url: "https://uncut.video/og.png",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "Uncut Video | Film & Photography",
+    card: "summary_large_image",
+  },
+  icons: {
+    shortcut: "/favicon.png",
+  },
+};
+
+const inter = Inter({
+	subsets: ["latin"],
+	variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+	src: "../public/fonts/CalSans-SemiBold.ttf",
+	variable: "--font-calsans",
+});
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+      <head>
+      </head>
+      <body
+        className={`bg-black ${
+          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+        }`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Particles from "./components/particles";
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+        <nav className="my-16 animate-fade-in"></nav>
+        <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        <Particles
+          className="absolute inset-0 -z-10 animate-fade-in"
+          quantity={100}
+        />
+        <h2 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-wrap bg-clip-text ">
+          PROCESSING
+        </h2>
+        <h2 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-wrap bg-clip-text ">
+          VIDEO<span className="animate-loading text-white"> . </span>
+          <span className="animate-loading delay-200 text-white"> . </span>
+          <span className="animate-loading delay-400 text-white"> . </span>
+        </h2>
+
+        <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        <div className="my-16 text-center animate-fade-in">
+          <h2 className="text-sm text-zinc-500 "></h2>
+        </div>
+      </div>
+    </>
+  );
+}
